a5c06682325b48dfd6ec5dc201d66c98a93bca3c
twmartin1147 <twmartin1147@gmail.com>
2018-03-24 12:58:01 -0500

    MySQL

---
+++ mySQLSchema.sql
@@ -0,0 +1,14 @@
+USE Bamazon;
+
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("AmazonBasics Mid-Back Mesh Chair", "Home & Kitchen", 64.99, 36);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("AmazonBasics Baker's Rack, Wood/Chrome", "Home & Kitchen", 59.99, 54);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("AmazonBasics Three Piece Glass Desk, Black with Black Glass", "Home & Kitchen", 109.99, 7);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("AmazonBasics Vanity Set with Stool, Small, Brown", "Home & Kitchen", 92.09, 25);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("Monopoly - The Classic Edition", "Toys, Kids & Baby", 21.97, 25);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("Aluminum Shock Mount For Blue Yeti Microphone by Auphonix", "Musical Instruments", 16.97, 17);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("ASUS ZS550KL ZenFone 3 Deluxe Unlocked Dual SIM Cell Phone, 5.5-inch 4GB RAM, 64GB storage, US Warranty (Glacier Silver)", "Cell Phones & Accessories", 444.87, 4);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("Razer Phone-120 Hz Ultra Motion Display-65GB Memory-8GB RAM-Dual Camera-Dual Front-Facing Speakers", "Cell Phones & Accessories", 699.99, 3116);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("Ticwatch S Smartwatch-Knight,1.4 inch OLED Display, Android Wear 2.0,Compatible with iOS and Android, Google Assistant", "Amazon Launchpad", 199.99, 47);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("Fitbit Wireless Wristband", "Electronics & Computers", 125.89, 24274);
+
+select * from Products;
